0f20f47e4313dca769ba6d84973853eb9535a7f0
YusukeEbisawa <gyuunyuu9832@gmail.com>
2023-10-10 23:41:47 +0900

    master作成

---
+++ KasaIruApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KasaIruApp
+//
+//  Created by 海老沢優典 on 2023/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
